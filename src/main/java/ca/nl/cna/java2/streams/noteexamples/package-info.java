57667cebab281d6663446b7f699568c0b5af5a02
josh.taylor <josh.taylor@cna.nl.ca>
2025-05-29 10:31:08 -0230

    Updated with Objective 2

---
+++ src/main/java/ca/nl/cna/java2/streams/noteexamples/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Examples from the PDF notes for the course
+ *
+ * Example 1: CopyBytes
+ * @see ca.nl.cna.java2.streams.noteexamples.CopyBytes
+ *
+ *
+ */
+package ca.nl.cna.java2.streams.noteexamples;
